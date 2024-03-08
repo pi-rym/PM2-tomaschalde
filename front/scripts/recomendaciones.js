dee02af7724323ac4ff6eefd56f29517c16e566a
tomaschalde <chalde_tomas@hotmail.es>
2024-03-07 23:10:07 -0300

    Homework - Clase 3

--- front/scripts/recomendaciones.js
+++ front/scripts/recomendaciones.js
@@ -19,3 +19,24 @@ document.addEventListener("DOMContentLoaded", function() {
       event.stopPropagation();
     });
 });
+
+//Funcion autoinvocada. Se invoca cuando en el documento html se llega a la etiqueta script. Ahorramos el hecho de poner una funcion y llamarla dentro de este codigo
+(function (){
+
+  const form = document.querySelector('#form')
+
+  form.addEventListener('submit', (evento) => { //Cuando el usuario hace click en el boton submit, realiza esto
+
+      if(!form.checkValidity()){ //Si el formulario no esta validado
+          
+          //Frenamos todo el proceso
+          evento.preventDefault() 
+          evento.stopPropagation()  
+
+          //Agregamos esta clase de bootstrap para informar que el formulario no esta validado (esta clase es de bootstrap)
+          form.classList.add('was-validated')
+      }
+      else
+          alert('Formulario Enviado')
+  })  
+}) ()
